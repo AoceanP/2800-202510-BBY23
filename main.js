b8f271c6818e0e027be0c6211e34a97d84bb33a6
Just-Iced <plunkettjustice@gmail.com>
2025-04-29 11:23:35 -0700

    Created the basic file structure

--- main.js
+++ main.js
@@ -15,15 +15,17 @@ mongoose.connect(mongoURI)
         console.log('Connected to MongoDB');
     })
     .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
+        //console.error('Error connecting to MongoDB:', err);
     });
 
 // Serve static files from "frontend" folder
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use("/img", express.static(path.join(__dirname + "/public/img")));
+app.use("/css", express.static(path.join(__dirname + "/public/css")));
+app.use("/js", express.static(path.join(__dirname + "/public/js")));
 
 // Default route - send index.html if user visits "/"
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start server
---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", event => {
+    window.location.href = "/login";
+});
+
+const signupBtn = document.getElementById("signup");
+signupBtn.addEventListener("click", event => {
+    window.location.href = "/signup";
+});
