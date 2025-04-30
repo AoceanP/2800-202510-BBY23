69e025483ddcc109ff95c3830c173ebf1056fee5
DeuceyMan789 <plunkettjustice@gmail.com>
2025-04-29 22:03:09 -0700

    Added logging in, and connections to mongo db

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const path = require('path');
 const bcrypt = require('bcrypt');
-const Uid = require('uid');
 const joi = require('joi');
 
 const app = express();
@@ -20,27 +19,14 @@ app.use("/img", express.static(path.join(__dirname + "/public/img")));
 app.use("/css", express.static(path.join(__dirname + "/public/css")));
 app.use("/js", express.static(path.join(__dirname + "/public/js")));
 
-mongoose.connect(mongoURI)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-    });
-
 const mongoStore = MongoStore.create({
     mongoUrl: mongoURI
 });
 
-const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, maxLength: 50},
-    username: {type: String, required: true, maxLength: 20},
-    password: {type: String, required: true, maxLength: 20},
-    name: String
-});
-const userModel = mongoose.model("user", userSchema);
-
-
+const client = new MongoClient(mongoURI);
+client.connect();
+const db = client.db("travelplanner");
+const users = db.collection("users");
 
 app.use(session({
     resave: false,
@@ -48,15 +34,23 @@ app.use(session({
     store: mongoStore,
     secret: process.env.SESSION_SECRET
 }));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Default route - send index.html if user visits "/"
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    if (req.session.user) {
+        res.redirect("/planner");
+    } else {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    }
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    if (req.session.user) {
+        res.redirect("/planner");
+    } else {
+        res.sendFile(path.join(__dirname, 'public', 'login.html'));
+    }
 });
 
 app.post('/loginUser', (req, res) => {
@@ -68,14 +62,63 @@ app.post('/loginUser', (req, res) => {
     if (query.error) {
         res.redirect("/login");
     } else {
-        //TODO: replace with login code
-        mongoose.model("users").find
+        users.findOne({ email: req.body.email }).then(user => {
+            console.log(user);
+            if (!user) {
+                res.redirect("/signup");
+            } else if (bcrypt.compareSync(req.body.password, user.password)) {
+                user.password = "";
+                req.session.user = user;
+                res.redirect("/planner");
+            }
+        });
+    }
+});
+
+app.get("/planner", (req, res) => {
+    if (req.session.user) {
+        res.send("Hello " + req.session.user.name);
+    } else {
         res.redirect("/");
     }
 });
 
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+    if (req.session.user) {
+        res.redirect("/planner");
+    } else {
+        res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+    }
+});
+
+app.post('/signupUser', async (req, res) => {
+    const schema = joi.object({
+        email: joi.string().required().email(),
+        username: joi.string().required(),
+        name: joi.string().required(),
+        password: joi.string().required().min(5).max(20),
+        password2: joi.string().required().min(5).max(20)
+    });
+    let query = schema.validate(req.body);
+    if (query.error) {
+        res.redirect("/signup");
+    } else {
+        
+        if (await users.findOne({ email: req.body.email })) {
+            res.redirect("/login");
+        } else {
+            let newUser = {
+                email: req.body.email,
+                username: req.body.username,
+                name: req.body.name,
+                password: await bcrypt.hash(req.body.password, saltRounds)
+            };
+            await users.insertOne(newUser);
+            newUser.password = "";
+            req.session.user = newUser;
+            res.redirect("/planner");
+        }
+    }
 });
 
 // Start server
